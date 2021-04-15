afd2949c4779630f0be9c8589031e22644f424c4
vnck <ivanckj96@gmail.com>
2021-04-15 22:57:44 +0800

    updated timeline and formatting

--- js/data-charts.js
+++ js/data-charts.js
@@ -10,31 +10,94 @@
 //     offset: -3,
 // });
 
+Chart.defaults.font.family = "'Quicksand', sans-serif !important;";
+
 const months = ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug','Sep','Oct','Nov','Dec'];
 
-const timeseries_options = {
-    scales: {
-        x: {
-            type: 'time',
-            time: {
-                unit: 'month'
-            }
-        }
-    },
-    plugins: {
-        tooltip: {
-            callbacks: {
-                title: (context) => {
-                    t = context[0].raw.x.split('-');
-                    return `${months[parseInt(t[1])-1]} ${t[0]}`
-                }
-            }
-        }
-    },
-}
 
 window.onloadFuncs.push(async () => {
     let data = await d3.json('data/monthyear_aggregated_tracetogether_reactions.json');
+    let headlines = await d3.json('data/tracetogether_reactions.json');
+
+    let scatterChartHeadlines = new Chart(
+        document.getElementById('scatterChartHeadlines'), {
+            plugins: [ChartDataLabels],
+            type: 'scatter',
+            data : {
+                datasets: [{
+                    label: 'Headlines',
+                    data: headlines.map(d => ({y: d.date, x: d.count, title: d.title, key_title: d.key_title})),
+                    borderColor: headlines.map(d => d.key_title ? 'palevioletred' : 'mediumaquamarine'),
+                    backgroundColor: headlines.map(d => d.key_title ? 'palevioletred' : 'mediumaquamarine'),
+                }]
+            },
+            options: {
+                aspectRatio: 1,
+                display: 'auto',
+                layout: {
+                    padding: {
+                        right: 300,
+                    }
+                },
+                scales: {
+                    y: {
+                        type: 'timeseries',
+                        reverse: true,
+                        offset: true,
+                        time: {
+                            unit: 'month',
+                            stepSize: 1,
+                        },
+                        title: {
+                            display: true,
+                            text: 'Date of Publication',
+                        },
+                    },
+                    x: {
+                        display: false,
+                        title: {
+                            display: true,
+                            text: 'No. of Articles',
+                        },
+                        type: 'linear',
+                        grid: {
+                            display: false,
+                        },
+                    }
+                },
+                plugins: {
+                    datalabels: {
+                        anchor: 'center',
+                        align: 'right',
+                        offset: (context) => {
+                            return 180 - context.dataset.data[context.dataIndex].x * 15; 
+                          },
+                        clip: false,
+                        formatter: (value, context) => {
+                          return context.dataset.data[context.dataIndex].key_title;
+                        }
+                    },
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                t = context[0].raw.y.split('-');
+                                return `${parseInt(t[2])} ${months[parseInt(t[1])-1]} ${t[0]}`
+                            },
+                            label: function(context) {
+                                let data = context.dataset.data[context.dataIndex];
+                                let title = data.title;
+                                let t = data.y.split('-');
+                                return `${parseInt(t[2])} ${months[parseInt(t[1])-1]} ${t[0]}: ${title}`;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
 
     let lineChartArticles = new Chart(
         document.getElementById('lineChartArticles'), {
@@ -44,11 +107,39 @@ window.onloadFuncs.push(async () => {
                     label: 'No. of Articles',
                     data: data.map(d => ({x: d.monthyear, y: Number(d.n)})),
                     borderColor: 'mediumaquamarine',
+                    backgroundColor: 'mediumaquamarine',
                     fill: false,
                     tension: 0.3,
                 }],
             },
-            options: timeseries_options
+            options: {
+                aspectRatio: 1.8,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'No. of Articles Published Per Month',
+                    },
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                t = context[0].raw.x.split('-');
+                                return `${months[parseInt(t[1])-1]} ${t[0]}`
+                            }
+                        }
+                    }
+                },
+            }
         }
     )
 
@@ -60,17 +151,49 @@ window.onloadFuncs.push(async () => {
                     label: 'Comments',
                     data: data.map(d => ({x: d.monthyear, y: Number(d.n_comments)})),
                     borderColor: 'deepskyblue',
+                    backgroundColor: 'deepskyblue',
                     fill: false,
                     tension: 0.3,
                 },{
                     label: 'Likes',
                     data: data.map(d => ({x: d.monthyear, y: Number(d.like)})),
                     borderColor: 'royalblue',
+                    backgroundColor: 'royalblue',
                     fill: false,
                     tension: 0.3,
                 }],
             },
-            options: timeseries_options
+            options: {
+                aspectRatio: 1.5,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Total No. of Likes and Comments Per Month'
+                    },
+                    legend: {
+                        labels: {
+                            boxWidth: 20,
+                            boxHeight: 20,
+                        }
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                t = context[0].raw.x.split('-');
+                                return `${months[parseInt(t[1])-1]} ${t[0]}`
+                            }
+                        }
+                    }
+                },
+            }
         }
     )
 
@@ -78,10 +201,10 @@ window.onloadFuncs.push(async () => {
         document.getElementById('barChartSentiments'), {
             type: 'bar',
             data: {
-                labels: ['Like','Haha','Love','Care','Wow','Sad','Angry'],
+                labels: ['Like','Love','Haha','Care','Wow','Sad','Angry'],
                 datasets: [{
                     label: 'Reactions',
-                    data: [52230,6261,6598,498,1326,516,2433],
+                    data: [52230,6598,6261,498,1326,516,2433],
                     backgroundColor: ['royalblue','purple','mediumorchid','palevioletred','orange','tomato','crimson'],
                 }],
             },
@@ -91,7 +214,16 @@ window.onloadFuncs.push(async () => {
                         display: true,
                         type: 'logarithmic',
                     }
-                }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Total No. of Reactions',
+                    },
+                    legend: {
+                        display: false
+                    },
+                },
             }
         }
     )
@@ -101,20 +233,14 @@ window.onloadFuncs.push(async () => {
             type: 'line',
             data: {
                 datasets: [{
-                    label: 'Like',
-                    data: data.map(d => ({x: d.monthyear, y: Number(d.like)})),
-                    borderColor: 'royalblue',
-                    backgroundColor: 'royalblue',
-                    tension: 0.3,
-                },{
-                    label: 'Haha',
-                    data: data.map(d => ({x: d.monthyear, y: Number(d.haha)})),
+                    label: 'Love',
+                    data: data.map(d => ({x: d.monthyear, y: Number(d.love)})),
                     borderColor: 'purple',
                     backgroundColor: 'purple',
                     tension: 0.3,
                 },{
-                    label: 'Love',
-                    data: data.map(d => ({x: d.monthyear, y: Number(d.love)})),
+                    label: 'Haha',
+                    data: data.map(d => ({x: d.monthyear, y: Number(d.haha)})),
                     borderColor: 'mediumorchid',
                     backgroundColor: 'mediumorchid',
                     tension: 0.3,
@@ -144,7 +270,37 @@ window.onloadFuncs.push(async () => {
                     tension: 0.3,
                 }],
             },
-            options: timeseries_options
+            options: {
+                aspectRatio: 1.5,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'No. of Reactions Per Month'
+                    },
+                    legend: {
+                        labels: {
+                            boxWidth: 20,
+                            boxHeight: 20,
+                        }
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                t = context[0].raw.x.split('-');
+                                return `${months[parseInt(t[1])-1]} ${t[0]}`
+                            }
+                        }
+                    }
+                },
+            }
         }
     )
 
@@ -153,14 +309,14 @@ window.onloadFuncs.push(async () => {
             type: 'line',
             data: {
                 datasets: [{
-                    label: 'Haha',
-                    data: data.map(d => ({x: d.monthyear, y: Number(d.haha)})),
+                    label: 'Love',
+                    data: data.map(d => ({x: d.monthyear, y: Number(d.love)})),
                     borderColor: 'purple',
                     backgroundColor: 'purple',
                     tension: 0.3,
                 },{
-                    label: 'Love',
-                    data: data.map(d => ({x: d.monthyear, y: Number(d.love)})),
+                    label: 'Haha',
+                    data: data.map(d => ({x: d.monthyear, y: Number(d.haha)})),
                     borderColor: 'mediumorchid',
                     backgroundColor: 'mediumorchid',
                     tension: 0.3,
@@ -172,7 +328,37 @@ window.onloadFuncs.push(async () => {
                     tension: 0.3,
                 }],
             },
-            options: timeseries_options
+            options: {
+                aspectRatio: 1.8,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'No. of Reactions Per Month'
+                    },
+                    legend: {
+                        labels: {
+                            boxWidth: 20,
+                            boxHeight: 20,
+                        }
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                t = context[0].raw.x.split('-');
+                                return `${months[parseInt(t[1])-1]} ${t[0]}`
+                            }
+                        }
+                    }
+                },
+            }
         }
     )
 
@@ -200,7 +386,37 @@ window.onloadFuncs.push(async () => {
                     tension: 0.3,
                 }],
             },
-            options: timeseries_options
+            options: {
+                aspectRatio: 1.8,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'No. of Reactions Per Month'
+                    },
+                    legend: {
+                        labels: {
+                            boxWidth: 20,
+                            boxHeight: 20,
+                        }
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                t = context[0].raw.x.split('-');
+                                return `${months[parseInt(t[1])-1]} ${t[0]}`
+                            }
+                        }
+                    }
+                },
+            }
         }
     )
 
@@ -216,7 +432,37 @@ window.onloadFuncs.push(async () => {
                     tension: 0.3,
                 }],
             },
-            options: timeseries_options
+            options: {
+                aspectRatio: 1.8,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'No. of Reactions Per Month'
+                    },
+                    legend: {
+                        labels: {
+                            boxWidth: 20,
+                            boxHeight: 20,
+                        }
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                t = context[0].raw.x.split('-');
+                                return `${months[parseInt(t[1])-1]} ${t[0]}`
+                            }
+                        }
+                    }
+                },
+            }
         }
     )
 });
--- js/scroll-color.js
+++
@@ -1,8 +0,0 @@
-
-window.onloadFuncs.push(() => {
-    var $target = $('.wrapper');
-inView('.colordiv').on('enter', function(el){
-    var color = $(el).attr('data-background-color');
-    $target.css('background-color', color );
-});
-});
