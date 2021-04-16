dc3a017460c69cb54d6c4aba7081df74ee810666
vnck <ivanckj96@gmail.com>
2021-04-16 10:44:12 +0800

    added  new aggregations

--- js/data-charts.js
+++ js/data-charts.js
@@ -195,6 +195,33 @@ window.onloadFuncs.push(async () => {
         }
     )
 
+    let optionsSums = [
+        {name: 'Sum', value: 'sum'},
+        {name: 'Mean', value: 'mean'},
+    ]
+
+    d3.select('#selectMenuLikes')
+        .selectAll('myOptions')
+        .data(optionsSums)
+        .enter()
+        .append('option')
+        .text(d => d.name)
+        .attr('value', d => d.value);
+
+    d3.select('#selectMenuLikes').on('change', d => {
+        let selectedOption = d3.select('#selectMenuLikes').property('value');
+        if (selectedOption === 'sum') {
+            lineChartLikesAndComments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.n_comments)}));
+            lineChartLikesAndComments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.like)}));
+            lineChartLikesAndComments.options.plugins.title.text = 'Total No. of Likes and Comments Per Month';
+        } else if (selectedOption === 'mean') {
+            lineChartLikesAndComments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.n_comments)/Number(d.n)}));
+            lineChartLikesAndComments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.like)/Number(d.n)}));
+            lineChartLikesAndComments.options.plugins.title.text = 'Mean No. of Likes and Comments Per Month';
+        }
+        lineChartLikesAndComments.update();
+    })
+
     let barChartSentiments = new Chart(
         document.getElementById('barChartSentiments'), {
             type: 'bar',
@@ -225,6 +252,14 @@ window.onloadFuncs.push(async () => {
             }
         }
     )
+
+    d3.select('#selectMenuSent')
+        .selectAll('myOptions')
+        .data(optionsSums)
+        .enter()
+        .append('option')
+        .text(d => d.name)
+        .attr('value', d => d.value);
 
     let lineChartSentiments = new Chart(
         document.getElementById('lineChartSentiments'), {
@@ -302,6 +337,36 @@ window.onloadFuncs.push(async () => {
         }
     )
 
+    d3.select('#selectMenuSent').on('change', d => {
+        let selectedOption = d3.select('#selectMenuSent').property('value');
+        if (selectedOption === 'sum') {
+            lineChartSentiments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.love)}));
+            lineChartSentiments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.haha)}));
+            lineChartSentiments.data.datasets[2].data =  data.map(d => ({x: d.monthyear, y: Number(d.care)}));
+            lineChartSentiments.data.datasets[3].data =  data.map(d => ({x: d.monthyear, y: Number(d.wow)}));
+            lineChartSentiments.data.datasets[4].data =  data.map(d => ({x: d.monthyear, y: Number(d.sad)}));
+            lineChartSentiments.data.datasets[5].data =  data.map(d => ({x: d.monthyear, y: Number(d.angry)}));
+            lineChartSentiments.options.plugins.title.text = 'Total No. of Reactions Per Month';
+        } else if (selectedOption === 'mean') {
+            lineChartSentiments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.love)/Number(d.n)}));
+            lineChartSentiments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.haha)/Number(d.n)}));
+            lineChartSentiments.data.datasets[2].data =  data.map(d => ({x: d.monthyear, y: Number(d.care)/Number(d.n)}));
+            lineChartSentiments.data.datasets[3].data =  data.map(d => ({x: d.monthyear, y: Number(d.wow)/Number(d.n)}));
+            lineChartSentiments.data.datasets[4].data =  data.map(d => ({x: d.monthyear, y: Number(d.sad)/Number(d.n)}));
+            lineChartSentiments.data.datasets[5].data =  data.map(d => ({x: d.monthyear, y: Number(d.angry)/Number(d.n)}));
+            lineChartSentiments.options.plugins.title.text = 'Mean No. of Reactions Per Month';
+        }
+        lineChartSentiments.update();
+    })
+
+    d3.select('#selectMenuPos')
+        .selectAll('myOptions')
+        .data(optionsSums)
+        .enter()
+        .append('option')
+        .text(d => d.name)
+        .attr('value', d => d.value);
+
     let lineChartPosSentiments = new Chart(
         document.getElementById('lineChartPosSentiments'), {
             type: 'line',
@@ -360,6 +425,30 @@ window.onloadFuncs.push(async () => {
         }
     )
 
+    d3.select('#selectMenuPos').on('change', d => {
+        let selectedOption = d3.select('#selectMenuPos').property('value');
+        if (selectedOption === 'sum') {
+            lineChartPosSentiments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.love)}));
+            lineChartPosSentiments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.haha)}));
+            lineChartPosSentiments.data.datasets[2].data =  data.map(d => ({x: d.monthyear, y: Number(d.care)}));
+            lineChartPosSentiments.options.plugins.title.text = 'Total No. of Reactions Per Month';
+        } else if (selectedOption === 'mean') {
+            lineChartPosSentiments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.love)/Number(d.n)}));
+            lineChartPosSentiments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.haha)/Number(d.n)}));
+            lineChartPosSentiments.data.datasets[2].data =  data.map(d => ({x: d.monthyear, y: Number(d.care)/Number(d.n)}));
+            lineChartPosSentiments.options.plugins.title.text = 'Mean No. of Reactions Per Month';
+        }
+        lineChartPosSentiments.update();
+    })
+
+    d3.select('#selectMenuNeg')
+        .selectAll('myOptions')
+        .data(optionsSums)
+        .enter()
+        .append('option')
+        .text(d => d.name)
+        .attr('value', d => d.value);
+
     let lineChartNegSentiments = new Chart(
         document.getElementById('lineChartNegSentiments'), {
             type: 'line',
@@ -417,52 +506,22 @@ window.onloadFuncs.push(async () => {
             }
         }
     )
-
-    let lineChartWowSentiments = new Chart(
-        document.getElementById('lineChartWowSentiments'), {
-            type: 'line',
-            data: {
-                datasets: [{
-                    label: 'Wow',
-                    data: data.map(d => ({x: d.monthyear, y: Number(d.wow)})),
-                    borderColor: 'orange',
-                    backgroundColor: 'orange',
-                    tension: 0.3,
-                }],
-            },
-            options: {
-                aspectRatio: 1.8,
-                scales: {
-                    x: {
-                        type: 'time',
-                        time: {
-                            unit: 'month'
-                        }
-                    }
-                },
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'No. of Reactions Per Month'
-                    },
-                    legend: {
-                        labels: {
-                            boxWidth: 10,
-                            boxHeight: 10,
-                        }
-                    },
-                    tooltip: {
-                        callbacks: {
-                            title: (context) => {
-                                t = context[0].raw.x.split('-');
-                                return `${months[parseInt(t[1])-1]} ${t[0]}`
-                            }
-                        }
-                    }
-                },
-            }
+    
+    d3.select('#selectMenuNeg').on('change', d => {
+        let selectedOption = d3.select('#selectMenuNeg').property('value');
+        if (selectedOption === 'sum') {
+            lineChartNegSentiments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.wow)}));
+            lineChartNegSentiments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.sad)}));
+            lineChartNegSentiments.data.datasets[2].data =  data.map(d => ({x: d.monthyear, y: Number(d.angry)}));
+            lineChartNegSentiments.options.plugins.title.text = 'Total No. of Reactions Per Month';
+        } else if (selectedOption === 'mean') {
+            lineChartNegSentiments.data.datasets[0].data =  data.map(d => ({x: d.monthyear, y: Number(d.wow)/Number(d.n)}));
+            lineChartNegSentiments.data.datasets[1].data =  data.map(d => ({x: d.monthyear, y: Number(d.sad)/Number(d.n)}));
+            lineChartNegSentiments.data.datasets[2].data =  data.map(d => ({x: d.monthyear, y: Number(d.angry)/Number(d.n)}));
+            lineChartNegSentiments.options.plugins.title.text = 'Mean No. of Reactions Per Month';
         }
-    )
+        lineChartNegSentiments.update();
+    })
 
     let options = [
         {name: 'Comments', value: 'n_comments', color: 'mediumaquamarine'},
